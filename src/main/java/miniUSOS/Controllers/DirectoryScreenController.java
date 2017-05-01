870c2b66c07e0f269d51ffa294447a3da7054444
Kasia <kasi.skowronska@gmail.com>
2017-05-02 01:15:30 +0200

    Visual improvements: studentScreen

--- src/main/java/miniUSOS/Controllers/DirectoryScreenController.java
+++ src/main/java/miniUSOS/Controllers/DirectoryScreenController.java
@@ -1,8 +1,18 @@
 package miniUSOS.Controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import miniUSOS.Classes.Course;
 import miniUSOS.Controllers.AbstractController;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Kasia on 01.05.2017.
@@ -10,10 +20,36 @@ import miniUSOS.Controllers.AbstractController;
 public class DirectoryScreenController extends AbstractController {
 
     public AnchorPane mainPane;
+    public TableView<Course> courseTable;
+    public TableColumn<Course, String> idCol;
+    public TableColumn<Course, String> courseCol;
+    public TableColumn<Course, String> facultyCol;
+
+    private ObservableList<Course> data;
 
     @FXML
-    public void initialize(){
+    public void initialize() {
         mainField = mainPane;
         fxml = "/Screens/DirectoryScreen.fxml";
+        idCol.setCellValueFactory(new PropertyValueFactory<Course, String>("ects"));
+        courseCol.setCellValueFactory(new PropertyValueFactory<Course, String>("name"));
+        facultyCol.setCellValueFactory(new PropertyValueFactory<Course, String>("abundance"));
+
+        loadList();
     }
+
+
+    public void loadList(){
+        List<Course> courses = retrieveCourses();
+        ObservableList<Course> items = FXCollections.observableArrayList();
+        for (Course course : courses) {
+            items.add(course);
+        }
+        courseTable.setItems(items);
+    }
+
+    public void addCourse(ActionEvent actionEvent) throws IOException {
+        switchToNewCourse();
+    }
+
 }
--- src/main/java/miniUSOS/Controllers/StudentScreenController.java
+++ src/main/java/miniUSOS/Controllers/StudentScreenController.java
@@ -1,14 +1,35 @@
 package miniUSOS.Controllers;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import miniUSOS.Controllers.AbstractController;
+import miniUSOS.Classes.Course;
+import miniUSOS.Classes.Group;
+import miniUSOS.Context;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentScreenController extends AbstractController {
 
 
     public AnchorPane mainPane;
+    public ListView coursesList;
+    public TableColumn hourCol;
+    public TableColumn<Group, String> moCol;
+    public TableColumn<Group, String> tuCol;
+    public TableColumn<Group, String> wdCol;
+    public TableColumn<Group, String> thCol;
+    public TableColumn<Group, String> frCol;
 
+    protected String activeGroup;
 
 
     @FXML
@@ -16,10 +37,61 @@ public class StudentScreenController extends AbstractController {
 
         mainField = mainPane;
         fxml = "/Screens/StudentScreen.fxml";
+        viewMyCourses();
+        setSchedule();
+    }
+
+    private void viewMyCourses() {
+        loadList();
+        setListProperty();
+    }
+
+    public void loadList(){
+        List<Group> groups = new ArrayList<>(Context.getInstance().getLoggedStudent().getGroups());
+        ObservableList<Group> items = FXCollections.observableArrayList();
+        for (Group group : groups) {
+            items.add(group);
+        }
+        coursesList.setItems(items);
+    }
+
+    public void setListProperty(){
+        coursesList.setCellFactory(param -> new ListCell<Group>() {
+            @Override
+            protected void updateItem(Group item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty || item == null || item.getName() == null) {
+                    setText(null);
+                } else {
+                    setText(item.getName());
+                }
+            }
+        });
+        coursesList.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener() {
+                    @Override
+                    public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                        Group group = (Group) newValue;
+                        activeGroup = group.getName();
+                    }
+                });
     }
 
 
-
+    public void setSchedule(){
+        moCol.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        tuCol.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        wdCol.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        thCol.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        frCol.setCellValueFactory(new PropertyValueFactory<Group, String>("name"));
+        ObservableList<String> hours = FXCollections.observableArrayList();
+        for (int i = 8; i < 18; i++) {
+            String hour = String.valueOf(i);
+            hours.add(hour);
+        }
+        //hourCol.getColumns().addAll(hours);
+}
 
 
 }
